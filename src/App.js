cd69680b4cf8406604a1111663a76d20f75d7d00
Saket Suhane <71808341+saket-github-code@users.noreply.github.com>
2023-06-19 17:01:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Callbackhook from "./Components/Callback";
+import Memo from "./Components/Memo";
+import "./styles.css";
+
+export default function App() {
+  // return <Memo />;
+  return <Callbackhook />;
+}
---
+++ src/Components/Callback.jsx
@@ -0,0 +1,34 @@
+// The React useCallback Hook returns a memoized callback function.
+
+import { useCallback, useState } from "react";
+import Todos from "./Todo";
+
+const Callbackhook = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const addTodo = useCallback(() => {
+    setTodos((prev) => [...prev, `new Entry`]);
+  }, [todos]);
+
+  // const addTodo = () => {
+  //   setTodos((prev) => [...prev, `new Entry`]);
+  // };
+
+  return (
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+
+export default Callbackhook;
